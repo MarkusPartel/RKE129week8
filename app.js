94db5d5b22f28f4a1c95f376ed8616e141b2956d
Markus Pärtle <markus.partel@tktk.ee>
2025-12-23 13:13:39 +0200

    add week 12 files

---
+++ app.js
@@ -0,0 +1,25 @@
+const images = ["1.jpg","2.jpg","3.jpg"];
+const targetImage = document.querySelector('.profiili-pilt img');
+let i = 1;
+
+
+targetImage.addEventListener('click', () => {
+
+    targetImage.style.opacity = "0";
+
+    setTimeout(() =>{
+
+
+        if(i === images.length) {
+            i = 0
+        }
+
+        targetImage.src = 'images/' + images[i]; 
+        i++;
+
+        targetImage.style.opacity = "1"
+
+    }, 300);
+    
+
+});
